e84b151083a523b8cb035b84ba6b911a7582e37c
Kevin Huang <goliath7415@gmail.com>
2017-11-05 16:34:29 +0800

    Edit default view template content

--- yungching-web.Tests/Controllers/HomeControllerTest.cs
+++ yungching-web.Tests/Controllers/HomeControllerTest.cs
@@ -16,10 +16,10 @@ namespace yungching_web.Tests.Controllers
         public void Index()
         {
             // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Index() as ViewResult;
+            var result = controller.Index() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
@@ -29,26 +29,13 @@ namespace yungching_web.Tests.Controllers
         public void About()
         {
             // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
             // Act
-            ViewResult result = controller.About() as ViewResult;
+            var result = controller.About() as ViewResult;
 
             // Assert
             Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
         }
     }
 }
