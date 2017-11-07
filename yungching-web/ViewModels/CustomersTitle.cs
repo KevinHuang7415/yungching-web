6b08d34e31520bd554e93e5ad52c40b0073cbb3a
Kevin Huang <goliath7415@gmail.com>
2017-11-07 16:42:49 +0800

    Add a new ViewModel to count the amount of customer titles

---
+++ yungching-web/ViewModels/CustomersTitle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yungching_web.ViewModels
+{
+    public class CustomersTitle
+    {
+        public String ContactTitle { get; set; }
+
+        public int ContactTitleCount { get; set; }
+    }
+}
